8668277ee7c46dce14a9fccb6eb84695080dabd8
Ivysalt <retrodpc@users.noreply.github.com>
2018-03-09 12:34:14 -0500

    Syntax error
    forgot an end

--- plugins/harucore.rb
+++ plugins/harucore.rb
@@ -5,13 +5,19 @@ class Cinch::Harucore
 
   match /ping/, :method => :on_ping, :react_on => :channel
 
+  set :help, <<-HELP
+cinch echo <text>
+  Echoes <text> back to you.
+  HELP
+
   def on_ping(m)
     m.reply("Pong!")
   end
   
   def on_channel(m)
     if m.message =~ /^[Oo][Kk]/
-    m.reply "Oh, okay, #{m.user.nick}."
+      m.reply "Oh, okay, #{m.user.nick}."
+    end
   end
 
 end
