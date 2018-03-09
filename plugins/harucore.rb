8dc2860f10a0a2d26f63016088a7d23ba394b36e
Ivysalt <retrodpc@users.noreply.github.com>
2018-03-09 12:23:32 -0500

    Starting Harucore
    With the grand total of two reactions
    
    !ping will reply with pong
    and ok will reply with ok

---
+++ plugins/harucore.rb
@@ -0,0 +1,22 @@
+class Cinch::Harucore
+  include Cinch::Plugin
+  
+  listen_to :channel, :method => :on_channel
+
+  match /ping/, :method => :on_ping, :react_on => :channel
+
+  set :help, <<-HELP
+cinch ping <text>
+  Replies with "Pong!".
+  HELP
+
+  def on_ping(m)
+    m.reply("Pong!")
+  end
+  
+  def on_channel(m)
+    if m.message =~ /^[Oo][Kk]/
+    m.reply "Oh, okay, #{m.user.nick}."
+  end
+
+end
