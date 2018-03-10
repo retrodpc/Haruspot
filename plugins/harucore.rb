212e89fd06316c306999bbe2cdbc0136e0becf04
Ivysalt <retrodpc@hotmail.com>
2018-03-09 21:12:50 -0500

    Fixing erroneous help info

--- plugins/harucore.rb
+++ plugins/harucore.rb
@@ -6,8 +6,8 @@ class Cinch::Harucore
   match /ping/, :method => :on_ping, :react_on => :channel
 
   set :help, <<-HELP
-cinch echo <text>
-  Echoes <text> back to you.
+cinch ping
+  Replies with "Pong!"
   HELP
 
   def on_ping(m)
