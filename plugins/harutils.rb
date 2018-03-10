71c1c32e76a75bfd2d404b21f0a061b3043a91e7
Ivysalt <retrodpc@hotmail.com>
2018-03-10 12:31:09 -0500

    Adding informational comments

--- plugins/harutils.rb
+++ plugins/harutils.rb
@@ -1,3 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# = Cinch calculation utilities plugin
+# This plugin adds a bunch of calculation commands,
+# mostly pertinent to chiptune.
+# 
+# == Commands
+# (to be added later)
+#
+# == Dependencies
+# None.
+#
+# == Configuration
+# None.
+#
+# == Author
+# retrodpc (Ivysalt)
+#
+# == License
+# Copyright © 2018 Ivysalt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Calculation utilities plugin for Cinch.
 class Cinch::Harutils
   include Cinch::Plugin
 
