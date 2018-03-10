61c5cb4c40ac76a66af1ddcff78fd5070c5353f8
Ivysalt <retrodpc@hotmail.com>
2018-03-09 20:43:10 -0500

    Prettifying output
    - Error message is more clear
    - Changed formatting to %d

--- plugins/harutils.rb
+++ plugins/harutils.rb
@@ -13,13 +13,13 @@ cinch amkspeed <tempo>
   def on_amkspeed(m, tempo)
     tempo = Float(tempo) rescue nil
     if tempo.nil?
-      m.reply("Sorry, #{m.user.nick}. That's not a number I recognise.")
+      m.reply("Sorry, #{m.user.nick}. That's not a valid input.")
     else
       amkspeed = tempo * 256.0 / 625.0
       if (amkspeed.round) == amkspeed
-        m.reply("Given tempo #{tempo}, the AMK speed is %f." % [amkspeed])
+        m.reply("Given tempo #{tempo}, the AMK speed is %d." % [amkspeed])
       else
-        m.reply("The AMK speed is about %.0f. The yielded tempo would be %.3f BPM." % [amkspeed, amkspeed.round * 625.0 / 256.0])
+        m.reply("The AMK speed is about %d. The yielded tempo would be %.3f BPM." % [amkspeed, amkspeed.round * 625.0 / 256.0])
       end
     end
   end
